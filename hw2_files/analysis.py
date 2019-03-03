911d1cb7615a4f9c922043b520686e0a0254edbb
XFRCX <xramirezcruzfx@gmail.com>
2019-03-03 16:28:34 -0600

    All done Chief

--- hw2_files/analysis.py
+++ hw2_files/analysis.py
@@ -1,5 +1,8 @@
 from collections import OrderedDict
+import collections
+import string
 
+#function that returns the frequency of characters in cipher text in a dictinary
 def frequency(cipher_text):
     letter_Frequency = {}
     for letter in cipher_text:
@@ -10,18 +13,53 @@ def frequency(cipher_text):
     ord_letter_frequency = OrderedDict(sorted(letter_Frequency.items()))
     return ord_letter_frequency
 
+#function used to print plain text of cipher text
+def decode(cipher_text, alphaKey):
+    print(cipher_text.translate(str.maketrans(alphaKey, string.ascii_lowercase)))
 
 
 
 
-letterX = "x"
+
 cipher_text = input("Please enter the text to decipher\n")
 letter_Frequency = frequency(cipher_text)
-print(letter_Frequency)
-#copy and paste results to see analyze the letter frequency against the encrypted text
-print("\n\n")
-#(*)
+alphabet = ""
+#make a string of all the character found in the cipher text
+for i in letter_Frequency:
+    alphabet = ''.join(alphabet + i)
 
+print("\n\n-----Characters found in Cipher text------\n")
+print("\t{}".format(alphabet))
+print("------------------------------------------\n")
+
+
+print("---------***Frequency Analysis***---------\n")
+for key in letter_Frequency:
+    print("\t {} found {} ".format(key, letter_Frequency[key]))
+print("------------------------------------------\n")
+
+print("----Characters not found in cipher text---\n")
+for chr in string.ascii_lowercase:
+    if chr not in letter_Frequency :
+        print("\t ----> {}".format(chr))
+print("------------------------------------------\n")
+
+
+
+#alphabet mapping found after manully messing around with different mappings
+alphaKey = "sgquntivdaejrozhpyfclwxmkb"
+print("-----------The alphabet mapping-----------")
+for cipherChar, plainChar in zip(alphaKey, string.ascii_lowercase):
+    print("\t {} ----> {}".format(cipherChar, plainChar))
+print("------------------------------------------\n")
+
+print("-----------cipher text to plain text------\n")
+decode( cipher_text, alphaKey)
+print("\n------------------------------------------\n")
+
+
+'''The following are muliple attempts of manually trying to figure out the alphabet mapping'''
+'''there are shown to show my work'''
 # sub = cipher_text.replace("l", "\033[33mu\033[0m")
 # sub = sub.replace("j", "\033[33ml\033[0m")
 # sub = sub.replace("b", "\033[33mj\033[0m")
@@ -90,7 +128,7 @@ print("\n\n")
 
 
 
-sub = cipher_text.replace("b", "z")# b- j
+# sub = cipher_text.replace("b", "z")# b- j
 # sub = cipher_text.replace("v", "h")
 # sub = sub.replace("w", "v")
 # sub = sub.replace("x", "w")
@@ -102,34 +140,32 @@ sub = cipher_text.replace("b", "z")# b- j
 # sub = sub.replace("n", "e")
 # sub = sub.replace("o", "n")
 # sub = sub.replace("z", "o")
-sub = sub.replace("g", "b")
-sub = sub.replace("i", "g")
-sub = sub.replace("d", "i")
+# sub = sub.replace("g", "b")
+# sub = sub.replace("i", "g")
+# sub = sub.replace("d", "i")
 #sub = sub.replace("l", "u")#--
-sub = sub.replace("u", "d")
-sub = sub.replace("l", "u")#--
-sub = sub.replace("j", "l")
-sub = sub.replace("s", "a")
-sub = sub.replace("f", "s")
-sub = sub.replace("t", "f")
-sub = sub.replace("c", "t")
-sub = sub.replace("q", "c")
-sub = sub.replace("p", "q")
-sub = sub.replace("h", "p")
-sub = sub.replace("v", "h")
-sub = sub.replace("w", "v")
-sub = sub.replace("x", "w")
-sub = sub.replace("m", "x")
-sub = sub.replace("r", "m")
-sub = sub.replace("y", "r")
-sub = sub.replace("k", "y")
-sub = sub.replace("e", "k")
-sub = sub.replace("n", "e")
-sub = sub.replace("o", "n")
-sub = sub.replace("z", "o")
+# sub = sub.replace("u", "d")
+# sub = sub.replace("l", "u")#--
+# sub = sub.replace("j", "l")
+# sub = sub.replace("s", "a")
+# sub = sub.replace("f", "s")
+# sub = sub.replace("t", "f")
+# sub = sub.replace("c", "t")
+# sub = sub.replace("q", "c")
+# sub = sub.replace("p", "q")
+# sub = sub.replace("h", "p")
+# sub = sub.replace("v", "h")
+# sub = sub.replace("w", "v")
+# sub = sub.replace("x", "w")
+# sub = sub.replace("m", "x")
+# sub = sub.replace("r", "m")
+# sub = sub.replace("y", "r")
+# sub = sub.replace("k", "y")
+# sub = sub.replace("e", "k")
+# sub = sub.replace("n", "e")
+# sub = sub.replace("o", "n")
+# sub = sub.replace("z", "o")
 #sub = sub.replace("b", "z")
-
-
 # sub = sub.replace("a", "z")
 
 
@@ -154,18 +190,13 @@ sub = sub.replace("z", "o")
 # sub = sub.replace("v", "\033[33mh\033[0m")
 # sub = sub.replace("w", "\033[33mv\033[0m")
 # sub = sub.replace("x", "\033[33mw\033[0m")
-
 # sub = sub.replace("u", "\033[33md\033[0m")
 # sub = sub.replace("l", "\033[33mu\033[0m")
-# sub = sub.replace("j", "\033[33ml\033[0m")
 # sub = sub.replace("b", "\033[33mj\033[0m")
 # sub = sub.replace("g", "\033[33mb\033[0m")
 # sub = sub.replace("i", "\033[33mg\033[0m")
 # sub = sub.replace("d", "\033[33mi\033[0m")
 # sub = sub.replace("u", "\033[33md\033[0m")
-
-
-
 
 '''----------------------------------------'''
 
@@ -198,71 +229,5 @@ sub = sub.replace("z", "o")
 # sub = sub.replace("x", "w")
 # sub = sub.replace("y", "r")
 # sub = sub.replace("z", "o")
-
-
-
-
-
-
-
-
-
+#print(sub)
 '''----------------------------------------'''
-
-
-
-
-
-# sub = cipher_text.replace("s", "\033[33ma\033[0m")
-# sub = sub.replace("g", "\033[33mb\033[0m")
-# sub = sub.replace("q", "\033[33mc\033[0m")
-# sub = sub.replace("u", "\033[33md\033[0m")
-# sub = sub.replace("n", "\033[33me\033[0m")
-# sub = sub.replace("t", "\033[33mf\033[0m")
-# sub = sub.replace("i", "\033[33mg\033[0m")
-# sub = sub.replace("v", "\033[33mh\033[0m")
-# sub = sub.replace("d", "\033[33mi\033[0m")
-# sub = sub.replace("b", "\033[33mj\033[0m")
-# sub = sub.replace("e", "\033[33mk\033[0m")
-# sub = sub.replace("j", "\033[33ml\033[0m")
-# sub = sub.replace("r", "\033[33mm\033[0m")
-# sub = sub.replace("o", "\033[33mn\033[0m")
-# sub = sub.replace("z", "\033[33mo\033[0m")
-# sub = sub.replace("h", "\033[33mp\033[0m")
-# sub = sub.replace("p", "\033[33mq\033[0m")
-# sub = sub.replace("y", "\033[33mr\033[0m")
-# sub = sub.replace("f", "\033[33ms\033[0m")
-# sub = sub.replace("c", "\033[33mt\033[0m")
-# sub = sub.replace("l", "\033[33mu\033[0m")
-# sub = sub.replace("w", "\033[33mv\033[0m")
-# sub = sub.replace("x", "\033[33mw\033[0m")
-# sub = sub.replace("m", "\033[33mx\033[0m")
-# sub = sub.replace("k", "\033[33my\033[0m")
-# sub = sub.replace("a", "\033[33mz\033[0m")
-
-# sub = cipher_text.replace("a", "\033[33mz\033[0m")
-# sub = sub.replace("s", "\033[33ma\033[0m")
-# sub = sub.replace("f", "\033[33ms\033[0m")
-# sub = sub.replace("t", "\033[33mf\033[0m")
-# sub = sub.replace("c", "\033[33mt\033[0m")
-# sub = sub.replace("q", "\033[33mc\033[0m")
-# sub = sub.replace("p", "\033[33mq\033[0m")
-# sub = sub.replace("h", "\033[33mp\033[0m")
-# sub = sub.replace("v", "\033[33mh\033[0m")
-# sub = sub.replace("w", "\033[33mv\033[0m")
-# sub = sub.replace("x", "\033[33mw\033[0m")
-# sub = sub.replace("m", "\033[33mx\033[0m")
-# sub = sub.replace("r", "\033[33mm\033[0m")
-# sub = sub.replace("y", "\033[33mr\033[0m")
-# sub = sub.replace("k", "\033[33my\033[0m")
-# sub = sub.replace("e", "\033[33mk\033[0m")
-# sub = sub.replace("n", "\033[33me\033[0m")
-# sub = sub.replace("o", "\033[33mn\033[0m")
-# sub = sub.replace("z", "\033[33mo\033[0m")
-
-
-
-
-
-
-print(sub)
--- hw2_files/password_cracker.py
+++ hw2_files/password_cracker.py
@@ -9,89 +9,106 @@ import collections
 #hexdigest() returns the encoded dat n hexadecimal format
 '''--------------------------------------------------------------------'''
 
-# fuunction used to transform a word with a caesar cipher
-# it takes a string and the offset key
+# fuunction used to transform a word with a caesar cipher key
 def cipherUp(orginalString, key):
+    #create queue of lower case alphabet
     lowerCaseAlphabetList = collections.deque(string.ascii_lowercase)
+    #now rotate the queue with provided key
     lowerCaseAlphabetList.rotate(key)
+    #create a string with no spaces between of the rotated alphabet
     alphabetString = ''.join(list(lowerCaseAlphabetList))
+    #replace all characters with created rotaed alphabet string
     cipherString = orginalString.translate(str.maketrans(string.ascii_lowercase, alphabetString))
     return cipherString
 
+#function used to crack userName7 password -- substitution cipher
+def sub_hack_tool(dictString):
+    #cipher alphabet mapping
+    alphaKey = "sgquntivdaejrozhpyfclwxmkb"
+    #return string that has replace all plain text with cipher alphabet mapping
+    return dictString.translate(str.maketrans(string.ascii_lowercase, alphaKey))
+
 
 #use md5 to try to crack the password size of 32 characters
-def md5_hack_tool(passHash, SALT):
+def md5_hack_tool(passHash, SALT, SUB):
+    orgWord = ""
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
         while word:
           if(SALT == "yes"):
               word = word + "SALT"
-              print(word)
+          if(SUB == "yes"):
+              orgWord = word
+              word = sub_hack_tool(word)
           hashedWord = hashlib.md5(word.encode('utf-8'))
-          #print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
           if(hashedWord.hexdigest() == passHash):
-            print("***Password was cracked using md5 hash algorithm***")
-            print("password: {}".format(word))
-            exit()
+              if(SUB == "yes"):
+                  print("***Password was cracked using md5 hash algorithm***")
+                  print("password: {}".format(orgWord))
+                  exit()
+              print("***Password was cracked using md5 hash algorithm***")
+              print("password: {}".format(word))
+              exit()
           word = fp.readline().strip('\n')
         fp.close()
 
-#use sha1 to try to crack the password size of 40 characters
+#use sha1 to try to crack the password size of 40 characters -- used for leet speak only
 def sha1_hack_tool(passHash, SALT):
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
         while word:
-          word = word.replace('a', '4')
-          word = word.replace('c', '(')
-          word = word.replace('e', '3')
-          word = word.replace('l', '1')
-          word = word.replace('o', '0')
-          word = word.replace('s', '5')
-          hashedWord = hashlib.sha1(word.encode('utf-8'))
-          print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
-          if(hashedWord.hexdigest() == passHash):
-            print("***Password was cracked using sha1 hash algorithm***")
-            print("password: {}".format(word))
-            exit()
-          word = fp.readline().strip('\n')
+            #replace characters for leet speak before hashing word
+            word = word.replace('a', '4')
+            word = word.replace('c', '(')
+            word = word.replace('e', '3')
+            word = word.replace('l', '1')
+            word = word.replace('o', '0')
+            word = word.replace('s', '5')
+            hashedWord = hashlib.sha1(word.encode('utf-8'))
+            if(hashedWord.hexdigest() == passHash):
+              print("***Password was cracked using sha1 hash algorithm***")
+              print("password: {}".format(word))
+              exit()
+            word = fp.readline().strip('\n')
         fp.close()
 
 #use blake2s to try to crack the password size of 64 characters
 def blake2s_hack_tool(passHash,SALT, CIPHER):
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
-
         while word:
-          if(SALT == "yes"):
-             combinations = list(itertools.product([str(0),str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8), str(9)], repeat=5))
-             string_list = map(''.join, combinations)
-             for s in string_list:
-                 cWord = word + s
-                 print(cWord)
-                 hWord = hashlib.blake2s(cWord.encode('utf-8'))
-                 if(hWord.hexdigest() == passHash):
-                   print("***Password was cracked using blake2s hash algorithm with SALT***")
-                   print("password: {}".format(word))
-                   exit()
-          elif(CIPHER == "yes"):
-              newWord = word
-              for i in range(1, 26):
-                  cipherWord = cipherUp(newWord, i)
-                  print(cipherWord)
-                  cHWord = hashlib.blake2s(cipherWord.encode('utf-8'))
-                  if(cHWord.hexdigest() == passHash):
-                    print("***Password was cracked using blake2s hash algorithm with cipher***")
-                    print("password: {}".format(cipherWord))
-                    exit()
-          else:
-            hashedWord = hashlib.blake2s(word.encode('utf-8'))
-            #print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
-            if(hashedWord.hexdigest() == passHash):
-              print("***Password was cracked using blake2s hash algorithm***")
-              print("password: {}".format(word))
-              exit()
-
-          word = fp.readline().strip('\n')
+            if(SALT == "yes"):
+               #create a list of all the combinations
+               combinations = list(itertools.product([str(0),str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8), str(9)], repeat=5))
+               #create a string of all combinations with no spaces
+               string_list = map(''.join, combinations)
+               #iterate through all combinations for each word in the dictionary
+               for s in string_list:
+                   cWord = word + s
+                   hWord = hashlib.blake2s(cWord.encode('utf-8'))
+                   if(hWord.hexdigest() == passHash):
+                     print("***Password was cracked using blake2s hash algorithm with SALT***")
+                     print("password: {}".format(word))
+                     exit()
+            elif(CIPHER == "yes"):
+                #save the orginal word
+                newWord = word
+                #iterate throught all possible shift keys
+                for i in range(1, 26):
+                    #cipherUp returns string with new key shift
+                    cipherWord = cipherUp(newWord, i)
+                    cHWord = hashlib.blake2s(cipherWord.encode('utf-8'))
+                    if(cHWord.hexdigest() == passHash):
+                      print("***Password was cracked using blake2s hash algorithm with cipher***")
+                      print("password: {}".format(cipherWord))
+                      exit()
+            else:
+              hashedWord = hashlib.blake2s(word.encode('utf-8'))
+              if(hashedWord.hexdigest() == passHash):
+                print("***Password was cracked using blake2s hash algorithm***")
+                print("password: {}".format(word))
+                exit()
+            word = fp.readline().strip('\n')
         fp.close()
 
 #use sha256 to try to crack the password size of 64 characters
@@ -99,37 +116,41 @@ def sha256_hack_tool(passHash, SALT, CIPHER):
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
         while word:
-          if(SALT == "yes"):
-              combinations = list(itertools.product([str(0),str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8), str(9)], repeat=5))
-              string_list = map(''.join, combinations)
-              for s in string_list:
-                  cWord = word + s
-                  print(cWord)
-                  hWord = hashlib.sha256(cWord.encode('utf-8'))
-                  if(hWord.hexdigest() == passHash):
-                    print("***Password was cracked using sha256 hash algorithm with SALT***")
-                    print("password: {}".format(word))
-                    exit()
+            if(SALT == "yes"):
+                #create a list of all the combinations
+                combinations = list(itertools.product([str(0),str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8), str(9)], repeat=5))
+                #create a string of all combinations with no spaces
+                string_list = map(''.join, combinations)
+                #iterate through all combinations for each word in the dictionary
+                for s in string_list:
+                    cWord = word + s
+                    hWord = hashlib.sha256(cWord.encode('utf-8'))
+                    if(hWord.hexdigest() == passHash):
+                      print("***Password was cracked using sha256 hash algorithm with SALT***")
+                      print("password: {}".format(word))
+                      exit()
 
-          elif(CIPHER == "yes"):
-              newWord = word
-              for i in range(1, 26):
-                  cipherWord = cipherUp(newWord, i)
-                  print(cipherWord)
-                  cHWord = hashlib.sha256(cipherWord.encode('utf-8'))
-                  if(cHWord.hexdigest() == passHash):
-                    print("***Password was cracked using sha256 hash algorithm with cipher***")
-                    print("password: {}".format(cipherWord))
-                    exit()
-          else:
-              hashedWord = hashlib.sha256(word.encode('utf-8'))
-              #print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
-              if(hashedWord.hexdigest() == passHash):
-                print("***Password was cracked using sha256 hash algorithm***")
-                print("password: {}".format(word))
-                exit()
+            elif(CIPHER == "yes"):
+                #save the orginal word
+                newWord = word
+                #iterate throught all possible shift keys
+                for i in range(1, 26):
+                    #cipherUp returns string with new key shift
+                    cipherWord = cipherUp(newWord, i)
+                    cHWord = hashlib.sha256(cipherWord.encode('utf-8'))
+                    if(cHWord.hexdigest() == passHash):
+                      print("***Password was cracked using sha256 hash algorithm with cipher***")
+                      print("password: {}".format(cipherWord))
+                      exit()
+            else:
+                hashedWord = hashlib.sha256(word.encode('utf-8'))
+                if(hashedWord.hexdigest() == passHash):
+                  print("***Password was cracked using sha256 hash algorithm***")
+                  print("password: {}".format(word))
+                  exit()
 
-          word = fp.readline().strip('\n')
+
+            word = fp.readline().strip('\n')
         fp.close()
 
 #use sha512 to try to crack the password size of 128 characters
@@ -137,29 +158,24 @@ def sha512_hack_tool(passHash, CIPHER):
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
         while word:
-          if(CIPHER == "yes"):
-              newWord = word
-              for i in range(26):
-                  cipherWord = cipherUp(newWord, i)
-                  print(cipherWord)
-
-                  cHWord = hashlib.sha512(cipherWord.encode('utf-8'))
-                  print("comparing: {} to {}".format(cHWord.hexdigest(), passHash))
-                  if(cHWord.hexdigest() == passHash):
-                    print("***Password was cracked using sha512 hash algorithm with cipher***")
-                    print("password: {}".format(word))
-                    exit()
-              #exit()
-          else:
-             hashedWord = hashlib.sha512(word.encode('utf-8'))
-             #print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
-             if(hashedWord.hexdigest() == passHash):
-               print("***Password was cracked using sha512 hash algorithm***")
-               print("password: {}".format(word))
-               exit()
-             #print("{}".format(hashedWord.hexdigest()))
-          word = fp.readline().strip('\n')
-
+            if(CIPHER == "yes"):
+                newWord = word
+                #iterate throught all possible shift keys
+                for i in range(26):
+                    #cipherUp returns string with new key shift
+                    cipherWord = cipherUp(newWord, i)
+                    cHWord = hashlib.sha512(cipherWord.encode('utf-8'))
+                    if(cHWord.hexdigest() == passHash):
+                      print("***Password was cracked using sha512 hash algorithm with cipher***")
+                      print("password: {}".format(word))
+                      exit()
+            else:
+               hashedWord = hashlib.sha512(word.encode('utf-8'))
+               if(hashedWord.hexdigest() == passHash):
+                 print("***Password was cracked using sha512 hash algorithm***")
+                 print("password: {}".format(word))
+                 exit()
+            word = fp.readline().strip('\n')
         fp.close()
 
 
@@ -168,80 +184,32 @@ def blake2b_hack_tool(passHash, CIPHER):
     with open('dictionary.txt') as fp:
         word = fp.readline().strip('\n')
         while word:
-          if(CIPHER == "yes"):
-              newWord = word
-              for i in range(26):
-                  cipherWord = cipherUp(newWord, i)
-                  print(cipherWord)
-                  cHWord = hashlib.blake2b(cipherWord.encode('utf-8'))
-                  print("comparing: {} to {}".format(cHWord.hexdigest(), passHash))
-                  if(cHWord.hexdigest() == passHash):
-                    print("***Password was cracked using blake2b hash algorithm with cipher***")
-                    print("password: {}".format(cipherWord))
-                    exit()
-              #exit()
-          else:
-             hashedWord = hashlib.blake2b(word.encode('utf-8'))
-             #print("comparing: {} to {} ".format(hashedWord.hexdigest(), passHash))
-             if(hashedWord.hexdigest() == passHash):
-               print("***Password was cracked using blake2b hash algorithm***")
-               print("password: {}".format(word))
-               exit()
-             #print("{}".format(hashedWord.hexdigest()))
-          word = fp.readline().strip('\n')
-
+            hjhlhl
+            if(CIPHER == "yes"):
+                newWord = word
+                for i in range(26):
+                    cipherWord = cipherUp(newWord, i)
+                    print(cipherWord)
+                    cHWord = hashlib.blake2b(cipherWord.encode('utf-8'))
+                    print("comparing: {} to {}".format(cHWord.hexdigest(), passHash))
+                    if(cHWord.hexdigest() == passHash):
+                      print("***Password was cracked using blake2b hash algorithm with cipher***")
+                      print("password: {}".format(cipherWord))
+                      exit()
+            else:
+               hashedWord = hashlib.blake2b(word.encode('utf-8'))
+               if(hashedWord.hexdigest() == passHash):
+                 print("***Password was cracked using blake2b hash algorithm***")
+                 print("password: {}".format(word))
+                 exit()
+            word = fp.readline().strip('\n')
         fp.close()
 
 
 
 
 
-# print('Guaranteed:\n{}\n'.format(
-#     ', '.join(sorted(hashlib.algorithms_guaranteed))))
-#
-# print('Available:\n{}'.format(
-#     ', '.join(sorted(hashlib.algorithms_available))))
-# a = "a52821b2db64438d547c73a7a7d57777"
-# print(len(a))
-i = "sin dios"
-w = hashlib.blake2b(i.encode('utf-8'))
-# z = hashlib.blake2s(i.encode('utf-8'))
-# t = hashlib.sha1(i.encode('utf-8'))
-# u = hashlib.sha224(i.encode('utf-8'))
-# b = hashlib.sha256(i.encode('utf-8'))
-# c = hashlib.sha384(i.encode('utf-8'))
-# d = hashlib.sha3_224(i.encode('utf-8'))
-# e = hashlib.sha3_256(i.encode('utf-8'))
-# f = hashlib.sha3_384(i.encode('utf-8'))
-g = hashlib.sha3_512(i.encode('utf-8'))
-h = hashlib.sha512(i.encode('utf-8'))
-# j = hashlib.shake_128(i.encode('utf-8'))
-# k = hashlib.shake_256(i.encode('utf-8'))
-#
-#
-#print(w.hexdigest())
-print(len(w.hexdigest()))
-# print(len(z.hexdigest()))
-# print(len(t.hexdigest()))
-# print(len(u.hexdigest()))
-# print(len(b.hexdigest()))
-# print(len(c.hexdigest()))
-# print(len(d.hexdigest()))
-# print(len(e.hexdigest()))
-# print(len(f.hexdigest()))
-print(len(g.hexdigest()))
-print(len(h.hexdigest()))
-# print(len(j.hexdigest()))
-# print(len(k.hexdigest()))
-# word = "sinDios"
-# print(type(word))
-# combinations = list(itertools.product([str(0),str(1),str(2),str(3),str(4),str(5),str(6),str(7),str(8), str(9)], repeat=5))
-# string_list = map(''.join, combinations)
-# for s in string_list:
-#     print(word+s)
-#     word = word + s
-#     print(word)
-#     exit()
+
 
 
 
@@ -250,23 +218,23 @@ print(len(h.hexdigest()))
 #get the hash to crack
 passHash= input("Input the hash of the password to crack: ")
 
-#get the length of the hash
-print("{} has a length of {}".format(passHash, len(passHash)))
+#crack the password according to the len of the hash string
 if( len(passHash) == 32):
-    md5_hack_tool(passHash, "no")
-    md5_hack_tool(passHash, "yes")
+    md5_hack_tool(passHash, "no", "no")
+    md5_hack_tool(passHash, "yes", "no")
+    md5_hack_tool(passHash, "no", "yes")
 
 elif( len(passHash) == 40):
     sha1_hack_tool(passHash, "no")
     sha1_hack_tool(passHash, "yes")
 
 elif( len(passHash) == 64):
-    blake2s_hack_tool(passHash, "no", "no")
     sha256_hack_tool(passHash, "no", "no")
-    blake2s_hack_tool(passHash, "no", "yes")
+    blake2s_hack_tool(passHash, "no", "no")
     sha256_hack_tool(passHash, "no", "yes")
-    blake2s_hack_tool(passHash, "yes", "no")
+    blake2s_hack_tool(passHash, "no", "yes")
     sha256_hack_tool(passHash, "yes", "no")
+    blake2s_hack_tool(passHash, "yes", "no")
 
 elif( len(passHash) == 128):
     sha512_hack_tool(passHash, "no")
